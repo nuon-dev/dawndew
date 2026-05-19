7776b9afdb698f519cb20d8b6f76b2bfc50c35aa
iubns <iubns@naver.com>
2026-05-20 02:40:10 +0900

    update: 기본 코드 추가

--- App.tsx
+++ App.tsx
@@ -5,12 +5,22 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import {
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+  View,
+  Linking,
+} from 'react-native';
+import WebView, { WebViewMessageEvent } from 'react-native-webview';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import { useRef } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const allowList = ['nuon.iubns.net', 'kauth.kakao.com', 'talk-apps.kakao.com'];
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -24,13 +34,34 @@ function App() {
 }
 
 function AppContent() {
+  const webviewRef = useRef<null | WebView>(null);
   const safeAreaInsets = useSafeAreaInsets();
+
+  const uri = 'https://nuon.iubns.net/';
+
+  async function onMessage(event: WebViewMessageEvent) {
+    const tokenData = event.nativeEvent.data;
+    await AsyncStorage.setItem('token', tokenData);
+  }
 
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
+      <WebView
+        ref={webviewRef}
+        geolocationEnabled={true}
+        userAgent="Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1"
+        source={{ uri }}
+        onContentProcessDidTerminate={() => {
+          webviewRef.current?.reload();
+        }}
+        onMessage={onMessage}
+        onShouldStartLoadWithRequest={event => {
+          if (allowList.some(allow => event.url.includes(allow))) {
+            return true;
+          }
+          Linking.openURL(event.url);
+          return false;
+        }}
       />
     </View>
   );
